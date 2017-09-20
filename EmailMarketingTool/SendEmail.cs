fca5e2a8286fcd05e92ea4c8158d52e9c0b9cfe4
phucphanviet <phanphuc97.pp@gmail.com>
2017-09-20 18:48:29 +0700

    First commit

---
+++ EmailMarketingTool/SendEmail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailMarketingTool
+{
+    class SendEmail
+    {
+        public static void SendEmailMarketing(string toAddress,string fromAddress,string pass,string subject,string content ) 
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                //initialize info
+                mail.From = new MailAddress(fromAddress);
+                mail.To.Add(toAddress);
+                mail.Subject = subject;
+                mail.Body = content;
+                mail.IsBodyHtml = true;
+
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    //
+                    smtp.Credentials = new NetworkCredential(fromAddress, pass);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+        }
+    }
+}
